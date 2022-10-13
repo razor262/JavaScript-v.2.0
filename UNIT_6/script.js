cfd25bdc2ff30b60ddffe357f0d40c8e3255adf7
raoz262 <24.work@bk.ru>
2022-10-13 19:35:39 +0300

    this: my new commit

--- UNIT_6/script.js
+++ UNIT_6/script.js
@@ -6,3 +6,9 @@ Task 1
 где звездочки рисуются с помощью внутреннего цикла от 0 до 3, а _ с 
 помощью внешнего.
 */
+let out = document.querySelector('.out')
+
+for (let i = 0; i < 3; i++) {
+    out.innerHTML = '*'
+    console.log(out)
+}
