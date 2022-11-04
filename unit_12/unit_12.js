6aa090fa32876168f1aa9073edad87715a3b5bcf
raoz262 <24.work@bk.ru>
2022-11-04 19:42:37 +0300

    this: my new commit

--- unit_12/unit_12.js
+++ unit_12/unit_12.js
@@ -420,12 +420,15 @@ console.groupEnd();
 //  Т.е вы руками просто пишите массив такой, чтобы условия выполнялись.
 
 let a20 = [
-
+    0,
+    [0,[0,9]],
+    [0,0,18],
+    12
 ];
 
 console.group('Task 20 ================');
-// console.log(a20[1][1][1] == 9);
-// console.log(a20[2][2] == 18);
-// console.log(a20[3]== 12);
+console.log(a20[1][1][1] == 9);
+console.log(a20[2][2] == 18);
+console.log(a20[3]== 12);
 
 console.groupEnd();
