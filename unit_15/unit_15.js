bce6cccf9432422a0e2b8b97128e667518fc36af
raoz262 <24.work@bk.ru>
2022-11-16 04:39:46 +0300

    this: my new commit

--- unit_15/unit_15.js
+++ unit_15/unit_15.js
@@ -222,10 +222,32 @@ document.querySelector('.b-12').onclick = () => {
 // { "H" : 1, 'e' : 1, 'l' : 2, "o" : 2, " ": 1}
 
 let str13 = 'The name conjures up visions of plum pudding and Christmas punch quaint coaching inns and cozy firesides but also of orphaned and starving children';
-
-
+let result13 = ''
 const f13 = () => {
+    let array13 = Array.from(str13)
+    let set13 = new Set(array13)
+    let one13 = Array.from(set13)
+    console.log(typeof array13)
+    console.log(typeof set13) 
+    console.log(typeof one13)
 
+    for (const item of set13) {
+        console.log(item)
+        let sum13 = 1
+
+            for (let i = 0; i < array13.length; i++) {
+                //const element = array13[i];
+                //console.log(element)
+
+                if (item == array13[i]) {
+                    sum13 += 1
+                    console.log( array13[i] + ' : ' + sum13)
+                    result13 += array13[i] + ' : ' + sum13 + ' '
+                }
+            }
+            
+    }
+        console.log(result13)
     // return
 }
 
