8ed72788108b917de529bed36ac9c70864dd70a0
raoz262 <24.work@bk.ru>
2022-11-24 14:35:07 +0300

    this: fix bug

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -17,9 +17,13 @@ document.querySelector('.b-1').onclick = t1;
 /*  Дан массив a1 = [2,3,4,5,10,11,12] - с помощью forEach переберите массив и
  создайте новый массив a2_res куда добавьте элементы данного массива деленные на 2. Действия должны
 запускаться при вызове функции t2.*/
-
+a2_res = []
 function t2() {
-
+    let a2 = [2,3,4,5,10,11,12]
+    a2.forEach(elem => {
+        a2_res.push(elem % 2)
+    })
+    document.querySelector('.out-2').innerHTML = a2_res
 }
 
 document.querySelector('.b-2').onclick = t2;
